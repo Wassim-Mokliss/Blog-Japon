ed0367e54af5facebf746cd2e217043c91031c2c
Wassim-Mokliss <wasshimudev@gmail.com>
2023-04-05 09:54:09 +0200

    création svelte header+menu+footer + sass menu

---
+++ src/App.svelte
@@ -0,0 +1,15 @@
+<script>
+  import Header from "./lib/Header.svelte";
+  
+  
+
+
+  console.log(import.meta.env.VITE_URL_DIRECTUS)
+</script>
+<Header/>
+
+<main>
+  
+</main>
+
+
---
+++ src/lib/Header.svelte
@@ -0,0 +1,30 @@
+<!-- On importe le menu pour le header -->
+<script>
+    import Menu from "./menu.svelte";
+    import bandeau from "../assets/Banniere header V1.jpg"
+    
+</script>
+
+<!-- On place l'image pour le bandeau du header -->
+<header>
+     <Menu/>
+    <div aria-hidden="true" class="bandeau">
+        <img src="{bandeau}" alt="bannière header"> 
+    </div>
+</header>
+
+<style>
+img {
+
+    max-width:50rem ;
+    display: flex;
+    
+    position: absolute;
+    top: 35%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+
+}
+
+
+</style>
