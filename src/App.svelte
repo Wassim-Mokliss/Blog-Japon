e5a86b9fcda7ed582bff07a3e3e4d2f0bda36c6c
Leez <alizeemalouda@gmail.com>
2023-04-14 19:59:20 +0200

    Page Articles.svelte + Page Article.svelte + routage app.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -2,6 +2,7 @@
       import Router from "svelte-spa-router";
       import Home from "./lib/Home.svelte";
       import Articles from "./lib/Articles.svelte";
+      import Article from "./lib/Article.svelte";
       import Contact from "./lib/Contact.svelte";      
       import Header from "./composants/Header.svelte";
       import Footer from "./composants/Footer.svelte";
@@ -9,15 +10,18 @@
       import Connexion from "./lib/Connexion.svelte";
       import A_propos from "./lib/A_propos.svelte";
       import Erreur from "./composants/Erreur.svelte";
-
+      import CreateArticle from "./lib/CreateArticle.svelte";
       const routes = {
       '/': Home,
       '/articles': Articles,
+      '/article/:id': Article,
+      '/create-article': CreateArticle,
       '/connexion' : Logister,
       '/contact' : Contact,
       '/login': Connexion,
       '/a-propos' : A_propos,
       '*': Erreur
+
       }
       
       //Test
--- src/lib/Articles.svelte
+++ src/lib/Articles.svelte
@@ -1,1 +1,24 @@
-<h1>bonjour sergei</h1>
+
+
+<script>
+    import axios from "axios"
+    let articles = [
+    
+    ]
+    //axios.get("http://localhost:8056/items/Article/1c6e944a-1894-4712-98fb-1d0233dd9b4b") => récupère un article en particulier par son ID
+    axios.get("http://localhost:8056/items/Article")
+    .then(function(resp){
+        articles = resp.data.data
+    })
+    .catch(function(error){
+        
+    })
+</script>
+
+{#each articles as article}
+<article>
+    <h2> {article.title}</h2>
+    <p>{article.description}</p>n
+</article>
+{/each}
+<style></style>
