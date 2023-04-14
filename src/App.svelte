b96c7fbbb45490cd5c57a392a1608343fed98f5b
Vlad <vlad.cirjan09@gmail.com>
2023-04-14 12:45:33 +0200

    Vlad - Correction - Suppression du fichier myapp

--- src/App.svelte
+++ src/App.svelte
@@ -2,20 +2,13 @@
       import Router from "svelte-spa-router";
       import Home from "./lib/Home.svelte";
       import Articles from "./lib/Articles.svelte";
-
-      import Contact from "./lib/Contact.svelte";
-
-      
+      import Contact from "./lib/Contact.svelte";      
       import Header from "./composants/Header.svelte";
       import Footer from "./composants/Footer.svelte";
       import Logister from "./lib/Logister.svelte";
       import Connexion from "./lib/Connexion.svelte";
       import A_propos from "./lib/A_propos.svelte";
       import Erreur from "./composants/Erreur.svelte";
-
-
-      
-
 
       const routes = {
       '/': Home,
